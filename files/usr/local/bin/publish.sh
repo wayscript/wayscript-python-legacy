fa622e571790e14b9431a55f921f5ea4accfd855
Jonathan Meier <jonathan@wayscript.com>
2021-07-14 11:15:15 -0400

    Write version to setup.py for publish

--- files/usr/local/bin/publish.sh
+++ files/usr/local/bin/publish.sh
@@ -3,11 +3,17 @@
 set -e
 set -x
 
+PROJECT_DIR=/usr/local/src/project
+
 # set version here. tag-version will blow the script up if it is not a clean working copy.
 VERSION=$(tag-version)
 export VERSION=$VERSION
 echo $VERSION
 
-DIST_DIR=/usr/local/src/project/dist
+VERSION_PLACEHOLDER="version='0.0.0',"
+sed -i "s/$VERSION_PLACEHOLDER/version='$VERSION',/" $PROJECT_DIR/setup.py
+
+
+DIST_DIR=$PROJECT_DIR/dist
 python setup.py sdist --dist-dir $DIST_DIR
 twine upload dist/wayscript-legacy-$VERSION.tar.gz
